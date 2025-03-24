5dad45f1817f16f769209b3c650dbe55deabc479
Bishal Lama <lb1627565@gmail.com>
2025-03-24 19:41:19 +0900

    first commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from './components/Header'
+
+const App=() => {
+  return (
+    <div className="container">
+      <Header title={'string'}/>
+    </div>
+  );
+}
+
+
+// class App extends React.Component{
+//   render(){
+//       return <h1>Hello doon</h1>
+//   }
+// }
+
+export default App;
